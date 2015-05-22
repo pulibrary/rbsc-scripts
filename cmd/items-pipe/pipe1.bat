33cd5a494e01d088d76933d3878e9b8a0d8710d3
heberlei <regineheberlein@gmail.com>
2015-05-22 10:32:20 -0400

    new tools to run XQueries for item creation in cmd environment

---
+++ cmd/items-pipe/pipe1.bat
@@ -0,0 +1,2 @@
+java -cp "C:\Program Files\Oxygen XML Editor 15\lib\saxon9ee.jar" net.sf.saxon.Query -update:on -t -q:"C:\Users\heberlei\Documents\SVN Working Copies\trunk\rbscXSL\Locations2015\AbID2dsc-items.xq" 
+2.bat
